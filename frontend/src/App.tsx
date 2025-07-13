3f8badf8c51707d741c1191b6a00e50df197f547
Šime Jadrijev <Sime.Jadrijev@infobip.com>
2025-07-13 16:56:29 +0200

    implement navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,13 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { LandingPage } from './pages/LandingPage';
 import { BlogPage } from './pages/BlogPage';
 import { AuthPage } from './pages/AuthPage';
+import { Navigation } from './components/Navigation';
 
 function App() {
 
     return (
         <div className={`${c}`}>
+            <Navigation/>
             <Routes>
                 <Route path="/" element={<Navigate to="/blogs"/>}/>
                 <Route path="/auth" element={<AuthPage/>}/>
---
+++ frontend/src/components/Navigation/Navigation.tsx
@@ -0,0 +1,12 @@
+import c from './index.module.css';
+import { Link } from 'react-router-dom';
+
+export const Navigation = () => {
+    return <div className={c.navigation}>
+        <nav>
+            <p><Link to={'/blogs'}>Blogs</Link></p>
+            <p><Link to={'/auth'}>Login/Register</Link></p>
+
+        </nav>
+    </div>;
+};
